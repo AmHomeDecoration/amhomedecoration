b76c8a68edb80a2b2a9126e944578f221e7343db
AmHomeDecoration <contact@amhomedecoration.com>
2025-03-14 17:22:22 +0100

    Update UserMenu.tsx

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -26,4 +26,33 @@ const UserMenu: React.FC = () => {
   );
 };
 
+import React from 'react';
+import { Home, Info, Folder, Settings } from 'lucide-react'; // Assurez-vous d'installer lucide-react ou utilisez vos propres icônes
+
+const UserMenu: React.FC = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <ul className="flex justify-around">
+        <li className="flex flex-col items-center">
+          <Home size={24} />
+          <span>Accueil</span>
+        </li>
+        <li className="flex flex-col items-center">
+          <Info size={24} />
+          <span>À propos</span>
+        </li>
+        <li className="flex flex-col items-center">
+          <Folder size={24} />
+          <span>Projets</span>
+        </li>
+        <li className="flex flex-col items-center">
+          <Settings size={24} />
+          <span>Paramètres</span>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default UserMenu;
 export default UserMenu;
